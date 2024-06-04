1692bf171f0be27f88e77a3c67796c6779203375
sp1aca9fa <sp1aca9fa@gmail.com>
2024-06-05 01:02:08 +0900

    Fundamentals Part 1 - variables, data types, basic operators - assignment start

---
+++ 01-Fundamentals-Part-1/assignments.js
@@ -0,0 +1,17 @@
+let country = "France";
+const continent = "Africa";
+let population = 50000000;
+
+console.log(country, continent, population);
+
+const isIsland = false;
+let language = "German";
+console.log(
+  typeof isIsland,
+  typeof population,
+  typeof country,
+  typeof language
+);
+
+language = "Hungary";
+isIsland = true;
