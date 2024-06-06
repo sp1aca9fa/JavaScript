ef9af6c42c84eac724f61b93c463030afe51d842
sp1aca9fa <sp1aca9fa@gmail.com>
2024-06-07 00:52:03 +0900

    Fundamentals Part 1 - operators precedence, strings and template literals, if and else statements, challenges 1 and 2

--- 01-Fundamentals-Part-1/assignments.js
+++ 01-Fundamentals-Part-1/assignments.js
@@ -2,16 +2,36 @@ let country = "France";
 const continent = "Africa";
 let population = 50000000;
 
-console.log(country, continent, population);
+// console.log(country, continent, population);
 
-const isIsland = false;
+// const isIsland = false;
 let language = "German";
-console.log(
-  typeof isIsland,
-  typeof population,
-  typeof country,
-  typeof language
-);
+// console.log(
+//   typeof isIsland,
+//   typeof population,
+//   typeof country,
+//   typeof language
+// );
 
-language = "Hungary";
-isIsland = true;
+// language = "Hungary";
+// isIsland = true;
+
+console.log(population / 2);
+population++;
+
+let finlandPopulation = 6000000;
+console.log(population > finlandPopulation);
+
+let averagePopulation = 33000000;
+console.log(population > averagePopulation);
+
+let description =
+  country +
+  " is in " +
+  continent +
+  " and its " +
+  population +
+  " people speak " +
+  language;
+
+console.log(description);
